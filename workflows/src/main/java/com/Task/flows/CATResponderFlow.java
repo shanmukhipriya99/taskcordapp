0259f50848cd0cbc3e22e983a8e009ce9dbde59a
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-04-21 15:34:21 +0530

    Added 8 flows

---
+++ workflows/src/main/java/com/Task/flows/CATResponderFlow.java
@@ -0,0 +1,27 @@
+package com.Task.flows;
+
+import co.paralleluniverse.fibers.Suspendable;
+import net.corda.core.flows.FlowException;
+import net.corda.core.flows.FlowLogic;
+import net.corda.core.flows.FlowSession;
+import net.corda.core.flows.InitiatedBy;
+
+// ******************
+// * Responder flow *
+// ******************
+@InitiatedBy(Initiator.class)
+public class CATResponderFlow extends FlowLogic<Void> {
+    private FlowSession counterpartySession;
+
+    public CATResponderFlow(FlowSession counterpartySession) {
+        this.counterpartySession = counterpartySession;
+    }
+
+    @Suspendable
+    @Override
+    public Void call() throws FlowException {
+        // Responder flow logic goes here.
+
+        return null;
+    }
+}
