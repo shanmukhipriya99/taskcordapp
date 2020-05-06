b2ea07c3877f2efd0d18b7a13d57a5843e52e5ee
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-05-06 11:51:58 +0530

    Added Flow code

--- workflows/src/main/java/com/Task/flows/CATInitiatorFlow.java
+++ workflows/src/main/java/com/Task/flows/CATInitiatorFlow.java
@@ -3,11 +3,8 @@ package com.Task.flows;
 import co.paralleluniverse.fibers.Suspendable;
 import com.Task.contracts.CATContract;
 import com.Task.states.TaskState;
-import com.sun.xml.bind.v2.model.core.ID;
-import net.corda.core.flows.FlowException;
-import net.corda.core.flows.FlowLogic;
-import net.corda.core.flows.InitiatingFlow;
-import net.corda.core.flows.StartableByRPC;
+import com.google.common.collect.ImmutableSet;
+import net.corda.core.flows.*;
 import net.corda.core.identity.Party;
 import net.corda.core.transactions.SignedTransaction;
 import net.corda.core.transactions.TransactionBuilder;
@@ -22,19 +19,40 @@ import java.time.Instant;
 @StartableByRPC
 public class CATInitiatorFlow extends FlowLogic<SignedTransaction> {
     private final Party Client;
+    private final Party mainContractor;
     private final Party subContractor;
     private final int taskID;
     private final String taskDesc;
     private final int amount;
     private final String assignee;
     private final Instant deadline;
-    private final ProgressTracker.Step Create_And_Assign_Task = new ProgressTracker.Step("Creating and assigning a task.");
-    private final ProgressTracker.Step Signing_Transaction = new ProgressTracker.Step("Signing transaction with our private key.");
-    private final ProgressTracker progressTracker = new ProgressTracker();
+    private final ProgressTracker.Step GENERATING_TRANSACTION = new ProgressTracker.Step("Creating and assigning task.");
+    private final ProgressTracker.Step VERIFYING_TRANSACTION = new ProgressTracker.Step("Verifying task constraints.");
+    private final ProgressTracker.Step SIGNING_TRANSACTION = new ProgressTracker.Step("Signing task with our private key.");
+    private final ProgressTracker.Step GATHERING_SIGS = new ProgressTracker.Step("Gathering the counterparty's signatures.") {
+        @Override
+        public ProgressTracker childProgressTracker() {
+            return CollectSignaturesFlow.Companion.tracker();
+        }
+    };
+    private final ProgressTracker.Step FINALISING_TRANSACTION = new ProgressTracker.Step("Recording task.") {
+        @Override
+        public ProgressTracker childProgressTracker() {
+            return FinalityFlow.Companion.tracker();
+        }
+    };
+    private final ProgressTracker progressTracker = new ProgressTracker(
+            GENERATING_TRANSACTION,
+            VERIFYING_TRANSACTION,
+            SIGNING_TRANSACTION,
+            GATHERING_SIGS,
+            FINALISING_TRANSACTION
+    );
 
 
-    public CATInitiatorFlow(Party Client, Party subContractor, int taskID, String taskDesc, int amount, String assignee, Instant deadline) {
+    public CATInitiatorFlow(Party Client, Party mainContractor, Party subContractor, int taskID, String taskDesc, int amount, String assignee, Instant deadline) {
         this.Client = Client;
+        this.mainContractor = mainContractor;
         this.subContractor = subContractor;
         this.taskID = taskID;
         this.taskDesc = taskDesc;
@@ -53,7 +71,31 @@ public class CATInitiatorFlow extends FlowLogic<SignedTransaction> {
     @Override
     public SignedTransaction call() throws FlowException {
         // Initiator flow logic goes here.
+        if (getOurIdentity().getName().getOrganisation().equals("mainContractor")) {
+            System.out.println("Main contractor is the initiator.");
+        } else {
+            throw new FlowException("Not initiated by the main contractor");
+        }
+        TaskState outputState = new TaskState(Client, getOurIdentity(), subContractor, taskID, taskDesc, amount, assignee, deadline);
+        //Stage1
+        progressTracker.setCurrentStep(GENERATING_TRANSACTION);
+        //Generating unsigned task
+        TransactionBuilder txBuilder = new TransactionBuilder(Client).addOutputState(outputState,ID).addCommand(new CATContract.Task(),getOurIdentity().getOwningKey());
+        //Stage2
+        progressTracker.setCurrentStep(VERIFYING_TRANSACTION);
+        txBuilder.verify(getServiceHub());
+        //Stage3
+        progressTracker.setCurrentStep(SIGNING_TRANSACTION);
+        //Sign the transaction
+        SignedTransaction TaskTxn = getServiceHub().signInitialTransaction(txBuilder);
+        //Stage4
+        progressTracker.setCurrentStep(GATHERING_SIGS);
+        //Send the state to counterparty and receive with signatures
+        FlowSession TaskSession = initiateFlow(subContractor);
+        SignedTransaction fullySignedTx = subFlow(new CollectSignaturesFlow(TaskTxn, ImmutableSet.of(TaskSession), CollectSignaturesFlow.Companion.tracker()));
+        //Stage5
+        progressTracker.setCurrentStep(FINALISING_TRANSACTION);
+        return subFlow(new FinalityFlow(TaskTxn, TaskSession));
 
-        return null;
     }
 }
